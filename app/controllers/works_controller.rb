a849d8635c44bf9825e78b1d1414f0ab2d5a6ddb
Kelly Downes <kedownes@gmail.com>
2019-04-23 20:21:12 -0700

    Add Create action, tests

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -8,5 +8,23 @@ class WorksController < ApplicationController
   end
 
   def create
+    @work = Work.new(work_params)
+
+    successful = @work.save
+    if successful
+      flash[:status] = :success
+      flash[:message] = "Successfully saved a work with ID #{@work.id}"
+      redirect_to works_path
+    else
+      flash.now[:status] = :error
+      flash.now[:message] = "Could not create #{@work.category}"
+      render :new, status: :bad_request
+    end
+  end
+
+  private
+
+  def work_params
+    return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
   end
 end
--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -16,4 +16,47 @@ describe WorksController do
       must_respond_with :success
     end
   end
+
+  describe "create" do
+    it "creates a new work" do
+      work_data = {
+        work: {
+          category: "book",
+          title: "Company",
+          creator: "Max Barry",
+          publication_year: 2007,
+          description: "From the outside, Zephyr is just another bland corporate monolith, but behind its glass doors business is far from usual: the sales reps use self help books as manuals, no one has seen the CEO, no one knows exactly what they are selling, and missing donuts are the cause of office intrigue.",
+        },
+      }
+
+      expect {
+        post works_path, params: work_data
+      }.must_change "Work.count", +1
+
+      must_respond_with :redirect
+      must_redirect_to works_path
+
+      check_flash
+
+      work = Work.last
+      expect(work.title).must_equal work_data[:work][:title]
+    end
+
+    it "sends back bad_request if no work data is sent" do
+      work_data = {
+        work: {
+          title: "",
+        },
+      }
+      expect(Work.new(work_data[:work])).wont_be :valid?
+
+      expect {
+        post works_path, params: work_data
+      }.wont_change "Work.count"
+
+      must_respond_with :bad_request
+
+      check_flash(:error)
+    end
+  end
 end
