1dbe04212851918167c269568aa1c6165a5bc865
Kelly Downes <kedownes@gmail.com>
2019-04-26 10:38:15 -0700

    Add User model and controller and login routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   root "homepages#index"
 
   resources :works
+
+  get "/login", to: "users#login_form", as: "login"
+  post "/login", to: "users#login"
+  post "/logout", to: "users#logout", as: "logout"
 end
