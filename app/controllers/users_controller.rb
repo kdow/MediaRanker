4ffee87597d812803abcc37bf272491206e19330
Kelly Downes <kedownes@gmail.com>
2019-04-26 10:52:05 -0700

    Add User login_form, login, and logout actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,30 @@
 class UsersController < ApplicationController
+  def login_form
+    @user = User.new
+  end
+
+  def login
+    username = params[:user][:username]
+
+    user = User.find_by(username: username)
+    unless user
+      user = User.create(username: username)
+    end
+
+    session[:user_id] = user.id
+
+    flash[:status] = :success
+    flash[:message] = "Successfully logged in as user #{user.username}"
+
+    redirect_to root_path
+  end
+
+  def logout
+    session[:user_id] = nil
+
+    flash[:status] = :success
+    flash[:message] = "Successfully logged out"
+
+    redirect_to root_path
+  end
 end
