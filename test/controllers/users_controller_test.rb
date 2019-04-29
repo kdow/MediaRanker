4cb502a35841a927a907d2c92c6387faa5aa2ac3
Kelly Downes <kedownes@gmail.com>
2019-04-28 20:44:18 -0700

    Add user controller tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,52 @@
 require "test_helper"
 
 describe UsersController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  before do
+    @user = User.first
+    @login_data = {
+      user: {
+        username: @user.username,
+      },
+    }
+  end
+
+  describe "login_form" do
+    it "can get the login page" do
+      get login_path
+
+      must_respond_with :success
+    end
+  end
+
+  describe "login" do
+    it "will login user" do
+      post login_path, params: @login_data
+
+      expect(session[:user_id]).must_equal @user.id
+    end
+
+    it "will respond with redirect" do
+      post login_path, params: @login_data
+
+      must_redirect_to root_path
+    end
+  end
+
+  describe "logout" do
+    before do
+      post login_path, params: @login_data
+    end
+
+    it "will logout user" do
+      post logout_path
+
+      expect(session[:user_id]).must_equal nil
+    end
+
+    it "will respond with redirect" do
+      post logout_path
+
+      must_redirect_to root_path
+    end
+  end
 end
