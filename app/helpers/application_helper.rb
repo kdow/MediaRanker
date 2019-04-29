a5274dcf62adb73ebfe9fde99f5d4a768ce67dee
Kelly Downes <kedownes@gmail.com>
2019-04-28 21:46:40 -0700

    Create index view for users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def readable_date(date)
+    return "[unknown]" unless date
+    return ("<span class='date' title='".html_safe +
+            date.to_s +
+            "'>".html_safe +
+            date.strftime("%b %d, %Y") +
+            "</span>".html_safe)
+  end
 end
