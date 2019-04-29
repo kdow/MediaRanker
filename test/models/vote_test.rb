0e1084c400da5c35b1bcac088e2c86f657bbb597
Kelly Downes <kedownes@gmail.com>
2019-04-28 18:31:05 -0700

    Add vote relation tests

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,13 +1,13 @@
 require "test_helper"
 
 describe Vote do
-  describe "validations" do
-    before do
-      @user = User.first
-      @work = Work.first
-      @vote = Vote.new(user_id: @user.id, work_id: @work.id)
-    end
+  before do
+    @user = User.first
+    @work = Work.first
+    @vote = Vote.new(user_id: @user.id, work_id: @work.id)
+  end
 
+  describe "validations" do
     it "is valid when all fields are present" do
       result = @vote.valid?
 
@@ -22,6 +22,16 @@ describe Vote do
 
       expect(result).must_equal false
       expect(duplicate_vote.errors.messages).must_include :user
+    end
+  end
+
+  describe "relations" do
+    it "has a user" do
+      expect(@vote.user_id).must_equal @user.id
+    end
+
+    it "has a work" do
+      expect(@vote.work_id).must_equal @work.id
     end
   end
 end
