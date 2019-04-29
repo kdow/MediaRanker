5a25be62952cca8e7cfd6984f77233e2dc33a67e
Kelly Downes <kedownes@gmail.com>
2019-04-28 17:56:29 -0700

    Add test for users voting on same work more than once

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,9 +1,27 @@
 require "test_helper"
 
 describe Vote do
-  let(:vote) { Vote.new }
+  describe "validations" do
+    before do
+      @user = User.first
+      @work = Work.first
+      @vote = Vote.new(user_id: @user.id, work_id: @work.id)
+    end
 
-  it "must be valid" do
-    value(vote).must_be :valid?
+    it "is valid when all fields are present" do
+      result = @vote.valid?
+
+      expect(result).must_equal true
+    end
+
+    it "rejects users voting on the same work more than once" do
+      @vote.save
+
+      duplicate_vote = Vote.new(user_id: @user.id, work_id: @work.id)
+      result = duplicate_vote.valid?
+
+      expect(result).must_equal false
+      expect(duplicate_vote.errors.messages).must_include :user
+    end
   end
 end
